c050c1b01c92499020579250686a1d758754c5d9
vue_lkk <1315637668.qq.com>
2023-03-07 17:40:07 +0800

    提交特效

---
+++ 渡一大师课/data.js
@@ -0,0 +1,53 @@
+var goods = [
+    {
+        pic: '瀑布流图片/1.webp',
+        title: "风景",
+        price: 32
+    },
+    {
+        pic: '瀑布流图片/2.webp',
+        title: "风景",
+        price: 12
+    },
+    {
+        pic: '瀑布流图片/3.webp',
+        title: "风景",
+        price: 56
+    },
+    {
+        pic: '瀑布流图片/4.webp',
+        title: "风景",
+        price: 102
+    }, 
+    {
+        pic: '瀑布流图片/5.webp',
+        title: "风景",
+        price: 72
+    }, 
+    {
+        pic: '瀑布流图片/6.webp',
+        title: "风景",
+        price: 32
+    }, 
+    {
+        pic: '瀑布流图片/7.webp',
+        title: "风景",
+        price: 66
+    }, 
+    {
+        pic: '瀑布流图片/8.webp',
+        title: "风景",
+        price: 5
+    }, 
+    {
+        pic: '瀑布流图片/9.webp',
+        title: "风景",
+        price: 9
+    }, 
+    {
+        pic: '瀑布流图片/10.webp',
+        title: "风景",
+        price: 88
+    }
+
+]
